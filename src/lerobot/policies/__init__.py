61ca094889f7e127f74d9106a5bdff52fe06c328
Ryoichi Nakajo <ryoichi-nakajo@aist.go.jp>
2025-09-18 20:48:55 +0900

    [act] add parties of vision encoders

--- src/lerobot/policies/__init__.py
+++ src/lerobot/policies/__init__.py
@@ -15,6 +15,9 @@
 from .act.configuration_act import ACTConfig as ACTConfig
 from .diffusion.configuration_diffusion import DiffusionConfig as DiffusionConfig
 from .groot.configuration_groot import GrootConfig as GrootConfig
+from .myact.configuration_act import ACTConfig as MyACTConfig
+from .dinoact.configuration_act import ACTConfig as DinoACTConfig
+from .siglipact.configuration_act import ACTConfig as SiglipACTConfig
 from .pi0.configuration_pi0 import PI0Config as PI0Config
 from .pi05.configuration_pi05 import PI05Config as PI05Config
 from .smolvla.configuration_smolvla import SmolVLAConfig as SmolVLAConfig
